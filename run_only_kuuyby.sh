a9e32975c349f8a313361f487b71103db0defd12
inikerin <nia@arenadata.io>
2024-02-01 15:09:56 +0300

    add beeline kuuybi test

---
+++ run_only_kuuyby.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+for number in 1 3 8 9 10 21 22 28 30 31 32 33 34 35 36 37 38 39 42 43 45 46 47 48 51 52 54 55 57 58 60 62 63 65 68 71 76 77 79 81 82 83 84 86 87 88 90 92 96 97 99
+do
+  echo "run query ${number}"
+  START_TIME=`date +%s`
+  beeline -n ppetrov -p 'SparkLight2023!' -u "jdbc:hive2://localhost:10009/tpcds_bin_partitioned_delta_10000" -f sample-queries-tpcds-spark/query${number}.sql
+  EXIT_STATUS=`echo $?`
+  if [[ $EXIT_STATUS == 0 ]]; then
+     EXIT_STATUS="OK"
+  else
+     EXIT_STATUS="ERROR"
+  fi
+  END_TIME=`date +%s`
+  RUNTIME=$((END_TIME-START_TIME))
+
+  echo \| sample-queries-tpcds-spark \| tpcds_bin_partitioned_delta_10000 \| ${RUNTIME} \| ${EXIT_STATUS}\| >> result_${TYPE_TEST}.txt
+
+done
--- sample-queries-tpcds-spark/query5.sql
+++ sample-queries-tpcds-spark/query5.sql
@@ -55,8 +55,7 @@ with ssr as
      date_dim,
      catalog_page
  where date_sk = d_date_sk
-       and d_date between cast('2000-08-19' as date)
-                  and (cast('2000-08-19' as date) +  14 days)
+       and d_date between cast('2000-08-19' as date) and date_add('2000-08-19', 14)
        and page_sk = cp_catalog_page_sk
  group by cp_catalog_page_id)
  ,
@@ -88,8 +87,7 @@ with ssr as
      date_dim,
      web_site
  where date_sk = d_date_sk
-       and d_date between cast('2000-08-19' as date)
-                  and (cast('2000-08-19' as date) +  14 days)
+       and d_date between cast('2000-08-19' as date) and date_add('2000-08-19', 14)
        and wsr_web_site_sk = web_site_sk
  group by web_site_id)
   select  channel
